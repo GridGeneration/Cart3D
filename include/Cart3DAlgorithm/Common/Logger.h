105e7f3f5ef950a0b4f8f3cc64d2d284f61cf61f
Administrator <Administrator@MS-LOQFLAVIMLTI>
2022-07-16 11:41:11 +0800

    添加日志库，这里日志库选用开源spdlog

---
+++ include/Cart3DAlgorithm/Common/Logger.h
@@ -0,0 +1,22 @@
+#ifndef CART3DALGORITHM_LOGGER_H
+#define CART3DALGORITHM_LOGGER_H
+#include "CommonConfig.h"
+#include <string>
+namespace Cart3DAlgorithm
+{
+
+	class COMMON_API Logger
+	{
+	public:
+		
+
+	private:
+		std::string path;
+		int model;
+	};
+
+
+}
+
+
+#endif

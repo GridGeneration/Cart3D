5263ea1d84b198f6f59194340fc3f09ad83d221c
Administrator <Administrator@MS-LOQFLAVIMLTI>
2022-07-17 15:48:22 +0800

    添加三角网格，这里放弃了支持复杂的数据类型，这里只引入stl数据

--- include/Cart3DAlgorithm/TriangleMesh/TriangleMesh.h
+++ include/Cart3DAlgorithm/TriangleMesh/TriangleMesh.h
@@ -3,15 +3,30 @@
 #include <vector>
 #include <array>
 #include "TriangleMeshConfig.h"
+#include <string>
 #include <Cart3DAlgorithm/Common/util.h>
 
 namespace Cart3DAlgorithm
 {
 
-	
+	class TRIANGLEMESH_API TriangleMesh
+	{
+	public:
+		TriangleMesh(std::vector<cvector3d>& pts,
+			std::vector<std::array<int, 3>>& face,
+			bool keepori);
+		//支持读取stl数据
+		TriangleMesh(const std::string& filename);
+	public:
+		cvector3d& get_point(int id);
+		const cvector3d& get_point(int id)const;
+		std::array<int, 3>& get_tri(int id);
+		const std::array<int, 3>& get_tri(int id)const;
+	private:
+		std::vector<cvector3d> pts;
+		std::vector<std::array<int, 3>> faces;
+	};
 }
-
-
 
 #endif // !CART3D_ALGORITHM_TRIANGLEMESH_H
 

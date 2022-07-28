0c3503465ff43bf003ecef71d18ced992c5760af
Administrator <Administrator@MS-LOQFLAVIMLTI>
2022-07-28 22:57:01 +0800

    测试浮点数转化为有理数效率，测试结果令人不满意。

--- Test/code/test_rational.cpp
+++ Test/code/test_rational.cpp
@@ -9,7 +9,11 @@ int main(int argc, char* argv[])
 	using frac64_t = rationals::rational<std::int64_t>;
 	frac64_t a(1212, 23256647);
 	frac64_t b(151552, 26598447);
-	frac64_t c = Rational::fromcfloat(0.333333333333);
+	clock_t st = clock();
+	frac64_t c;
+	for(int i=0;i<102456458;++i)
+		c = Rational::fromcfloat(-3.1415926537942);
+	std::cout << "Time:" << clock() - st << "ms" << std::endl;
 	std::cout << c << std::endl;
 	std::cout << (a < b) << std::endl;
 	return 0;
